0d27bbfbf0e7028a6f4fcc26a0bb658d3e31ef84
Brian Yu <brianyu28@gmail.com>
2017-09-22 10:55:20 -0400

    Add fourth check

--- square/check50/__init__.py
+++ square/check50/__init__.py
@@ -4,12 +4,20 @@ class Square(Checks):
 
     @check()
     def exists(self):
+        """square.c exists"""
         self.require("square.c")
 
     @check("exists")
     def compiles(self):
+        """square.c compiles"""
         self.spawn("clang -o square square.c").exit(0)
 
     @check("compiles")
     def squares_0(self):
+        """square.c computes square of 0 as 0"""
         self.spawn("./square 0").stdout("^0\n","0\n")
+
+    @check("compiles")
+    def squares_28(self):
+        """square.c computes square of 28 as 784"""
+        self.spawn("./square 0").stdout("^784\n","784\n")
